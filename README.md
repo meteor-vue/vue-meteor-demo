362a0930adee847fb9936ec9871a67428970e07f
Guillaume Chau <guillaume.b.chau@gmail.com>
2017-03-23 18:58:28 +0100

    Now SSR enabled

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 A comprehensive demo project, using Meteor &amp; Vue.
 
 This is a work in progress. Contribute by opening Issues and submitting PRs!
+
+```
+meteor create --bare <some_folder>
+cd <some_folder>
+meteor remove blaze-templates reactive-var
+meteor add static-html akryum:vue-component akryum:vue-ssr
+```
--- src/imports/server.js
+++ src/imports/server.js
@@ -1,1 +1,42 @@
 import './api/publications'
+
+import { VueSSR } from 'meteor/akryum:vue-ssr'
+import CreateApp from './app'
+
+// You can change the rendered HTML template
+
+/* VueSSR.template = `
+<div class="app-wrapper">
+  <!--vue-ssr-outlet-->
+</div>
+` */
+
+// Simple create app
+
+VueSSR.createApp = function ({ url }) {
+  const { app, router } = CreateApp()
+  // Set the URL in the router
+  router.push(url)
+  return app
+}
+
+// Returning a promise works too
+
+/* VueSSR.createApp = function ({ url }) {
+  return new Promise((resolve, reject) => {
+    const { app, router } = CreateApp()
+    // Set the URL in the router
+    router.push(url)
+
+    router.onReady(() => {
+      const matchedComponents = router.getMatchedComponents()
+
+      // no matched routes
+      if (!matchedComponents.length) {
+        reject({ code: 404 })
+      }
+
+      resolve(app)
+    })
+  })
+} */
