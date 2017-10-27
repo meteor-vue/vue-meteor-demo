b89401c28c8b9291b6e80f52bd29d6a06ab59197
Guillaume Chau <alphadelta.fg@gmail.com>
2017-10-27 17:57:20 +0200

    Update

--- src/imports/ui/Home.vue
+++ src/imports/ui/Home.vue
@@ -4,7 +4,7 @@
       <img src="https://github.com/Akryum/vue-meteor/raw/master/vue%2Bmeteor.png"/>
     </p>
     <p>Work in progress... <a href="https://github.com/Akryum/vue-meteor-demo">Contribute!</a></p>
-    <p>This application is a complete demo project  using Meteor and Vue. It features routing and SSR, and more (vuex, apollo, accounts...) will be added soon.</p>
+    <p>This application is a complete demo project using Meteor and Vue. It features routing and SSR, and more (vuex, apollo, accounts...) will be added soon.</p>
     <p><a href="https://github.com/Akryum/vue-meteor/" target="_blank">More info</a></p>
   </div>
 </template>
